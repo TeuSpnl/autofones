565269dc3fed2d404fae5ba7a629c502f98e4103
Mateus Spinola <teu_almeida20010@hotmail.com>
2024-02-05 18:08:56 -0300

    Changing ddos, create function print_timeout and wait_presence to reduce written code

--- autofones.py
+++ autofones.py
@@ -4,12 +4,14 @@ import pyautogui as ptgui
 import base64
 
 from selenium.webdriver import Edge
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.options import Options
 from selenium.common.exceptions import WebDriverException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 
 
 # Set some configurations to the browser
@@ -28,13 +30,48 @@ driver = Edge(options=opts)
 driver.set_page_load_timeout(2)
 
 
+def print_timeout_trying_next(i):
+    if i <= 25:
+        print(f"\n\nTimeout. Trying next: \n\n \
+                \n############################### \
+                \n###############################\n")
+        opening_page(i + 1)
+    else:
+        print(f"\n\nTimeout. Max tries. Shutting down: \n\n \
+                \n############################### \
+                \n###############################\n")
+        driver.quit()
+        quit()
+
+
+def wait_presence(elemento, i, continua=0):
+    # Waits for the page to load and then Open the IP settings page
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, f"{elemento}"))
+        )
+
+        if element:
+            pass
+
+    except TimeoutException as e:
+        if continua == 1:
+            print(f"\n\nFatal timeout error. Shutting Down. \n\n \
+                    \n############################### \
+                    \n###############################\n")
+            driver.quit()
+
+        else:
+            print_timeout_trying_next(i)
+
+
 def opening_page(start=12):
     """ Tries to open the page and log in. If it fails, it tries the next page.
 
     Args:
         url (int, optional): _description_. Defaults to 12.
     """
-    url = f"http://192.168.253.2{start:02d}/"
+    url = f"http://182.17.2.2{start:02d}/"
 
     try:
         # Set the login credentials as headers to the bwoser – This skips the pop up login page
@@ -45,24 +82,15 @@ def opening_page(start=12):
 
         # Open the browser and navigate to the page
         driver.get(url)
+        sleep(.5)
+        ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
     # If the page takes more than 2 seconds to load, tries the next link. If some another error occurs, quits the browser.
     except (WebDriverException, TimeoutException) as e:
-        print(f"\nTimeout error. Trying next: \n\n\
-                \n############################### \
-                \n############################### \
-                \n############################### \
-                \n###############################\n")
-        if start <= 25:
-            opening_page(start + 1)
-        else:
-            driver.quit()
-            quit()
+        print_timeout_trying_next(start)
 
     except Exception as e:
         print(f"\n\nUNKNOWN ERROR: \n\n{e} \
-                  \n############################### \
-                  \n############################### \
                   \n############################### \
                   \n###############################\n")
         driver.quit()
@@ -77,21 +105,9 @@ def opening_page(start=12):
         print(f"\nPrivacy error: {e}")
 
     # It waits for the page to load and then changes the DNS settings
-    try:
-        element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.ID, "label_operation_time"))
-        )
+    wait_presence("label_operation_time", start)
 
-        if element:
-            wan(start)
-
-    except TimeoutException as e:
-        print(f"\n\nTimeout. Trying next: \n\n \
-                  \n############################### \
-                  \n############################### \
-                  \n############################### \
-                  \n###############################\n")
-        opening_page(start + 1)
+    wan(start)
 
     while start <= 25:
         opening_page(start)
@@ -100,14 +116,12 @@ def opening_page(start=12):
 
 def restart():
     driver.find_element("id", "linkMenuRestart").click()
-    sleep(.5)
+    wait_presence("resetSystem", 0, 1)
     driver.find_element("id", "resetSystem").click()
     alert = driver.switch_to.alert
     alert.accept()
     try:
         print(f"\nAlert text: {alert.text} \
-                  \n############################### \
-                  \n############################### \
                   \n############################### \
                   \n###############################\n")
     except Exception as e:
@@ -128,7 +142,7 @@ def privacy_fix(driver):
 
 
 def wan(i):
-    # Open the IP settings page
+    wait_presence("linkMenuNetwork", i)
     driver.find_element("id", "linkMenuNetwork").click()
     sleep(.2)
     driver.find_element("id", "linkMenuNetworkWan").click()
@@ -139,33 +153,33 @@ def wan(i):
             EC.text_to_be_present_in_element_value((By.ID, "ETHAddressIP"), "")
         )
 
-        if not element:
-            print("timeout")
-            opening_page(i + 1)
+        if element:
+            pass
 
     except TimeoutException as e:
-        print(f"\n\nTimeout. Trying next: \n\n \
-                  \n############################### \
-                  \n############################### \
-                  \n############################### \
-                  \n###############################\n")
-        opening_page(i + 1)
+        print_timeout_trying_next(i)
 
-    change_ip(i)
-    change_dns()
+    # Put as DHCP
+    sleep(.5)
+    if not driver.find_element("id", "ETHActivateDHCPClient_static").is_selected():
+        driver.find_element("id", "ETHActivateDHCPClient_static").click()
+
+    change_ddos(i)
+
+    # change_ip(i)
+    # change_dns()
 
     # Save the changes and restart the phone
-    sleep(.5)
+    sleep(.2)
     driver.find_element("id", "saveButton").click()
-    sleep(1)
     restart()
 
 
 def change_ip(i):
 
-    ip_value = f"192.168.253.2{i}"
+    ip_value = f"182.17.2.2{i:02d}"
     mask_value = "255.255.255.0"
-    gateway_value = "192.168.253.254"
+    gateway_value = "182.17.2.254"
 
     # Upadate the IP settings and save
     ip = driver.find_element("id", "ETHAddressIP")
@@ -202,6 +216,46 @@ def change_dns():
     secondary.clear()
     primary.send_keys(DNSprima)
     secondary.send_keys(DNSsecun)
+
+
+def change_ddos(i):
+    # Change the tab to the advanced settings
+    driver.find_element("id", "li_Avançado").click()
+
+    # Waits for the page to load and then activate QoS settings
+    wait_presence("enable_qos_layer_3", i, 1)
+
+    # Activate the QoS settings
+    if not driver.find_element("id", "enable_qos_layer_3").is_selected():
+        driver.find_element("id", "enable_qos_layer_3").click()
+
+    # Find the QoS type elements
+    sip_element = driver.find_element("id", "sip_type")
+    sip_select = Select(sip_element)
+    rtp_element = driver.find_element("id", "rtp_type")
+    rtp_select = Select(rtp_element)
+    dados_element = driver.find_element("id", "data_type")
+    dados_select = Select(dados_element)
+
+    # Select the DSCP option for all the QoS types
+    sip_select.select_by_visible_text("DSCP")
+    rtp_select.select_by_visible_text("DSCP")
+    dados_select.select_by_visible_text("DSCP")
+
+    # Find the QoS value elements
+    sip_value = driver.find_element("id", "sip_value")
+    rtp_value = driver.find_element("id", "rtp_value")
+    dados_value = driver.find_element("id", "data_type_value")
+
+    # Clear the input fields and set the values
+    sip_value.clear()
+    rtp_value.clear()
+    dados_value.clear()
+
+    # Set the values as EF (Expedited Forwarding), or 46, for higher priority
+    sip_value.send_keys("46")
+    rtp_value.send_keys("46")
+    dados_value.send_keys("46")
 
 
 opening_page(1)
